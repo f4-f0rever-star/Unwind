1874bac6c184c923aade32d6ed530452e77111dc
Faizah <lolcanyouseeme@gmail.com>
2026-05-26 11:31:04 +0300

    Use deployed backend url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-export const API_BASE = 'http://localhost:5000';
+export const API_BASE = 'https://unwind-project.onrender.com';
 
 export async function apiRequest(path, options = {}) {
   const token = localStorage.getItem('token');
